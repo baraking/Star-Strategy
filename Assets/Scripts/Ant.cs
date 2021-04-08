1c8452f595d3a6960693d9e0de667b9d96a9d463
baraking <barakatz.bk@gmail.com>
2021-04-08 18:10:27 +0300

    improved the ant class

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ant : Walkable
+//Notice the collider is a sphere and not a cone or something.
+public class Ant : Gatherer
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject detectionRange;
+
+    private void Update()
     {
-        
+        if (targetResource != null)
+        {
+            WalkTowards();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        print(other);
+        if (targetResource == null && other.GetComponent<Resource>())
+        {
+            targetResource = other.GetComponent<Resource>();
+        }
+    }
+
+    void WalkTowards()
+    {
+        Vector3 targetDirection = targetPoint - transform.position;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, unitDetails.speed * Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, unitDetails.speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetPoint) < 0.1f)
+        {
+            hasTarget = false;
+        }
     }
 }
