cf234da9813046520ca795733199499fbccbad02
baraking <barakatz.bk@gmail.com>
2021-05-09 01:36:12 +0300

    need to change all weapon prefabs to add a weapon holder

---
+++ Assets/Scripts/WeaponHolder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder : MonoBehaviour
+{
+
+    public List<Purchasables> possibleUpgrades;
+
+    public void BuildUpgrade(Purchasables chosenPurchasable)
+    {
+
+    }
+
+}
