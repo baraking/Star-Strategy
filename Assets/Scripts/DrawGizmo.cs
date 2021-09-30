a04ca213f1ffd39fdde589a39c1a4ea3224507c2
baraking <barakatz.bk@gmail.com>
2021-09-30 18:26:46 +0300

    Drawing Resource Positions

--- Assets/Scripts/DrawGizmo.cs
+++ Assets/Scripts/DrawGizmo.cs
@@ -4,12 +4,50 @@ using UnityEngine;
 
 public class DrawGizmo : MonoBehaviour
 {
+    public static readonly float DIAMETER_CONST = 0.05f;
 
-    public float diameter;
+    public int color;
+    public int value;
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(transform.position, diameter);
+        if (color == 0)
+        {
+            Gizmos.color = Color.black;
+        }
+        if (color == 1)
+        {
+            Gizmos.color = Color.blue;
+        }
+        if (color == 2)
+        {
+            Gizmos.color = Color.cyan;
+        }
+        if (color == 3)
+        {
+            Gizmos.color = Color.gray;
+        }
+        if (color == 4)
+        {
+            Gizmos.color = Color.green;
+        }
+        if (color == 5)
+        {
+            Gizmos.color = Color.magenta;
+        }
+        if (color == 6)
+        {
+            Gizmos.color = Color.red;
+        }
+        if (color == 7)
+        {
+            Gizmos.color = Color.white;
+        }
+        if (color == 8)
+        {
+            Gizmos.color = Color.yellow;
+        }
+
+        Gizmos.DrawSphere(transform.position, value * DIAMETER_CONST);
     }
 }
---
+++ Assets/Scripts/ResourcePositions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePositions : MonoBehaviour
+{
+
+    public GameObject[] resourcePositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
